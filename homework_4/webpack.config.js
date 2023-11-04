7295033bb0a844e18a3ad8ffc5ba20239aeacb07
Artem <artem.egorov.art2006@gmail.com>
2023-11-04 15:51:33 +0300

    настроил webpack и scss

---
+++ homework_4/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
